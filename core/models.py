345387a3bb12b5a3f614bd99d7dda2e029319548
marcinfo <90268921+marcinfo@users.noreply.github.com>
2023-10-18 10:36:43 -0300

    v.1.1.1 202310181036

--- core/models.py
+++ core/models.py
@@ -93,28 +93,7 @@ class Tb_Registros(Base):
     def __str__(self):
         return self.usuario
 
-class Ocorrencias(Base):
-    crialista()
-    id_ocorrencia = models.AutoField(primary_key=True)
-    id_user = models.CharField(verbose_name='user_id',max_length=45)
-    user = models.CharField(verbose_name='Usuário',max_length=45)
-    data_registro =  models.DateField(verbose_name='Data da Ocorrência',help_text='Data em que foi visualizada a praga.')
-    praga = models.CharField(max_length=40,choices=lista_praga,help_text='Selecione qual o tipo de praga esta contaminando.')
-    cultura =  models.CharField(name='Cultura',max_length=48,choices=lista_cultura,help_text='Qual plantação foi contaminada?')
-    status = models.CharField(verbose_name='Controlada?', max_length=45, choices=CONTROLE_CHOICE,help_text='A praga esta controlada?')
-    nome_propriedade = models.CharField(verbose_name='Nome da Propriedade afetada',max_length=60,help_text='Nome da propriedade que esta sendo contaminada.')
-    prejuizo=models.DecimalField(verbose_name='Total do prejuizo R$',max_digits=20, decimal_places=2,default=0.0,help_text='qual o valor do prejuizo?')
-    hectares=models.IntegerField(verbose_name='Quantidade de hectar afetado',default=0,help_text='quantos hectares estão contaminados')
-    latitude = models.CharField(max_length=45)
-    longitude = models.CharField(max_length=45)
 
-    imagens = StdImageField('Imagem',upload_to='images',help_text='Selecione as imagens da praga.',null=True,blank=True)
-    observacao = models.TextField(name='Observações',null=True,blank=True)
-    class Meta:
-        verbose_name = "Tabela de Ocorrência"
-        verbose_name_plural = "Tabela de Ocorrência"
-    def __str__(self):
-        return self.user
 
 class TbParametros(models.Model):
     id= models.AutoField(primary_key=True)
