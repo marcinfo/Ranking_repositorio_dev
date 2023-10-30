6e1ce71f2178c1a5d18e7ee62072d47a5478aeca
marc_info <marc_info@yahoo.com.br>
2023-10-30 18:14:27 -0300

    validações em campos indicadores M

--- core/forms.py
+++ core/forms.py
@@ -12,11 +12,9 @@ class UserRegistrationForm(forms.ModelForm):
                                widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password',
                                 widget=forms.PasswordInput)
-
     class Meta:
         model = User
         fields = ('username', 'first_name', 'email')
-
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
